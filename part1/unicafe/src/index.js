6962b4ba8ad599ef622de260c22fd9b5f6a5b469
pedro.abalo <pedro.abalo.next@bbva.com>
2020-05-03 10:40:32 +0200

    1.7 unicafe, step2 resolved

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,22 +13,37 @@ const HeaderStatistics = () =>
 const Statistics = ({text,value}) =>
   <p>{text} {value}</p>
 
+const Positives = ({text, value}) =>
+  <p>{text} {value} % </p>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage((good+1)/(all+1))
+    setPositive((good+1) * 100.0 / (all+1))
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((good-bad)/(all+1))
+    setPositive(good * 100.0 / (all+1))
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all +1)
+    setAverage((good-(bad+1))/(all+1))
+    setPositive(good * 100.0 / (all+1))
   }
 
   return (
@@ -38,9 +53,12 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
       <HeaderStatistics />
-      <Statistics text='good' value={good}/>
-      <Statistics text='neutral' value={neutral}/>
-      <Statistics text='bad' value={bad}/>
+      <Statistics text='good' value={good} />
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
+      <Statistics text='all' value={all} />
+      <Statistics text='average' value={average} />
+      <Positives text='positive' value={positive} />
     </div>
   )
 }
